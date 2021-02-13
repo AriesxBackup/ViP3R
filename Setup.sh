cd301d85a159e6d4dc5363c76e3b0bc6500a7607
XCode <64787166+1337Xcode@users.noreply.github.com>
2021-02-13 23:57:20 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,2 +1,2 @@
 ## Created By @ERR0Rxx / @T34M3RR0Rz
-pkg upgrade -y && pkg install python -y && chmod +x * && chmod +x /module/* >/dev/null 2>/dev/null
+pkg upgrade -y && pkg install python -y && pkg install tsu -y && tsu && chmod +x * && chmod +x /module/* && exit >/dev/null 2>/dev/null
